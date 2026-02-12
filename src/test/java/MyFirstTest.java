9912ad1d070a3f6af024a5f8547b33875c5aa957
Inikolay <Ibraun12345@gmail.com>
2026-02-12 20:00:33 +0200

    add

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,39 +1,41 @@
-import com.microsoft.playwright.Browser;
+
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.junit.Options;
+import com.microsoft.playwright.junit.OptionsFactory;
+import com.microsoft.playwright.junit.UsePlaywright;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+
+@UsePlaywright(MyFirstTest.MyOption.class)
 public class MyFirstTest {
 
-    @Test
-    void myTest() {
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
+    public static class MyOption implements OptionsFactory{
 
-        page.navigate("https://practicesoftwaretesting.com/");
-
-        String title = page.title();
-
-        Assertions.assertTrue(title.contains("Practice Software Testing - Toolshop - v5.0"));
-
-
-        browser.close();
-        playwright.close();
+        @Override
+        public Options getOptions() {
+            return new Options().setHeadless(false)
+                    .setLaunchOptions(
+                            new BrowserType.LaunchOptions()
+                            .setArgs(Arrays.asList("--no-sandbox","--disable-gpu"))
+                    );
+        }
     }
 
     @Test
-    void shouldSearchByKeyword(){
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
+    void myTest(Page page) {
+        page.navigate("https://practicesoftwaretesting.com/");
+        String title = page.title();
+        Assertions.assertTrue(title.contains("Practice Software Testing - Toolshop - v5.0"));
+    }
 
+    @Test
+    void shouldSearchByKeyword(Page page) {
         page.navigate("https://practicesoftwaretesting.com/");
         page.locator("#search-query").fill("Pliers");
         page.locator("button[data-test='search-submit']").click();
-
-        browser.close();
-        playwright.close();
     }
 }
